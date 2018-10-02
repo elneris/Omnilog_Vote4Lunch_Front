09955fb052d8915b2af81fd5efed4282e02e9042
FabienSchlegel <fabien.schlegel@omnilog.fr>
2018-10-02 17:05:05 +0200

    ajout des routes

--- back/routes/index.js
+++ back/routes/index.js
@@ -30,5 +30,65 @@ router.get('/api/now', (req, res) => {
   });
 });
 
+router.get('/api/lunch/all', (req, res) => {
+  const sqlQuery = "SELECT * FROM lunch;";
+  connection.query(sqlQuery, (error, results) => {
+    if (error) {
+      res.status(500).json({ error });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+router.post('/api/lunch/add', (req, res) => {
+  const sqlQuery = `INSERT INTO lunch (username, place, date) values ('${req.body.username}', '${req.body.place}', '${req.body.date}');`
+  
+  connection.query( sqlQuery, (error) => {
+    if (error) {
+      res.status(500).json({ error });
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
+router.post('/api/lunch/up_vote', (req, res) => {
+
+  let voteForDate = '';
+
+  if (req.body.date !== '') {
+    voteForDate = ` AND date = '${req.body.date}'`
+  }
+
+  const sqlQuery = `UPDATE lunch SET vote = vote + 1 WHERE place = '${req.body.place}' ${voteForDate}`
+  
+  connection.query( sqlQuery, (error) => {
+    if (error) {
+      res.status(500).json({ error });
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
+router.post('/api/lunch/down_vote', (req, res) => {
+
+  let voteForDate = '';
+
+  if (req.body.date !== '') {
+    voteForDate = ` AND date = '${req.body.date}'`
+  }
+
+  const sqlQuery = `UPDATE lunch SET vote = vote - 1 WHERE place = '${req.body.place}' ${voteForDate}`
+  
+  connection.query( sqlQuery, (error) => {
+    if (error) {
+      res.status(500).json({ error });
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
 
 export default router;
