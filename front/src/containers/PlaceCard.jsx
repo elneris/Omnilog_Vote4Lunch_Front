f6d44a77a3b3ad3d1e0c19f44a0987b5fe754acb
FabienSchlegel <fabien.schlegel@omnilog.fr>
2018-10-09 14:03:35 +0200

    update addVoice components

--- front/src/containers/PlaceCard.jsx
+++ front/src/containers/PlaceCard.jsx
@@ -7,9 +7,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus, faMinus, faInfo } from '@fortawesome/free-solid-svg-icons'
 
 class PlaceCard extends Component {
-    constructor(props) {
-        super(props);
-    }
 
     render() {
 
--- front/src/containers/VoteMap.jsx
+++ front/src/containers/VoteMap.jsx
@@ -3,21 +3,9 @@ import { connect } from 'react-redux';
 
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
-import Controls from '../components/Controls';
-
 import MaterialIcon from 'material-icons-react';
 
 import 'leaflet/dist/leaflet.css';
-
-import L from 'leaflet';
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-    iconUrl: require('leaflet/dist/images/marker-icon.png'),
-    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-});
 
 class VoteMap extends Component {
 
@@ -72,7 +60,6 @@ class VoteMap extends Component {
                     <TileLayer
                         url={mapTiles}
                     />
-                    <Controls/>
 {
     restaurants.map(restaurant => (
         <Marker
