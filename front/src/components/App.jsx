2ce7f56a4f17d3552d72ddd4930b3c95c339cb5b
FabienSchlegel <fabien.schlegel@omnilog.fr>
2018-10-10 14:13:02 +0200

    update CSS

--- front/src/components/App.jsx
+++ front/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import BeginVote from './BeginVote'
 import AddPlace from './AddPlace'
@@ -11,15 +11,15 @@ import AddAVoice from '../containers/AddAVoice';
 const App = () => {
 
     return (
-
-        <Switch>
-          <Route exact path="/" component={BeginVote} />
-          <Route exact path="/begin-vote" component={CreateAVote} />
-          <Route exact path="/add-place" component={AddPlace} />
-          <Route exact path="/vote/:url" component={AddAVoice} />
-        </Switch>
-
-
+        <div className="App">
+        
+            <Switch>
+                <Route exact path="/" component={BeginVote} />
+                <Route exact path="/begin-vote" component={CreateAVote} />
+                <Route exact path="/add-place" component={AddPlace} />
+                <Route exact path="/vote/:url" component={AddAVoice} />
+            </Switch>
+        </div>
     );
 }
 
