eacdcbd376aad5c5e34758640c71b7b73da1c3f6
FabienSchlegel <fabien.schlegel@omnilog.fr>
2018-10-11 09:46:43 +0200

    update sequelize initialization

--- api/sequelize.js
+++ api/sequelize.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 import { PlaceModel } from './models/place';
-import  { VoteModel } from './models/vote';
-import { VoiceModel } from './models/voice'
+import { VoteModel } from './models/vote';
+import { VoiceModel } from './models/voice';
 import fs from 'fs';
 
 const sequelize = new Sequelize('vote4lunch', 'db_user', 'fakepassword', {
@@ -15,19 +15,25 @@ const sequelize = new Sequelize('vote4lunch', 'db_user', 'fakepassword', {
     }
 })
 
+// Model's initialization
+
 export const Place = PlaceModel(sequelize, Sequelize);
-
 export const Vote = VoteModel(sequelize, Sequelize);
-
 export const Voice = VoiceModel(sequelize, Sequelize);
+
+// Relationship between tables
 
 Voice.belongsTo(Vote);
 Voice.belongsTo(Place);
+
+// M2M relationship between Vote and Place
 
 let VotePlace = sequelize.define('vote_place', {});
 
 Vote.belongsToMany(Place, { through: VotePlace });
 Place.belongsToMany(Vote, { through: VotePlace });
+
+// Database initialization
 
 sequelize.sync({ force: true })
     .then(() => {
@@ -36,11 +42,13 @@ sequelize.sync({ force: true })
         const PlacesData = JSON.parse(fs.readFileSync('./.json_data/places.json', 'UTF-8'));
 
         PlacesData.map(place => Place
-            .create({
-                name: place.tags.name,
-                lat: parseFloat(place.lat),
-                lng: parseFloat(place.lon),
-                type: place.tags.amenity
+            .findOrCreate({
+                where: {
+                    name: place.tags.name,
+                    lat: parseFloat(place.lat),
+                    lng: parseFloat(place.lon),
+                    type: place.tags.amenity
+                }
             })
         )
     })
