bbc9c5373f50100ee385aa68acb2a4aba1730536
FabienSchlegel <fabien.schlegel@omnilog.fr>
2018-10-12 10:32:53 +0200

    add redux dev tools

--- front/src/index.js
+++ front/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import reduxThunk from 'redux-thunk';
 import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import allReducers from './reducers';
 
@@ -11,11 +12,9 @@ import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-const createStoreWithMiddleWare = applyMiddleware(reduxThunk)(createStore);
-const store = createStoreWithMiddleWare(
-  allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(allReducers, composeWithDevTools(
+  applyMiddleware(reduxThunk)
+));
 
 ReactDOM.render(
   <Provider store={store}>
