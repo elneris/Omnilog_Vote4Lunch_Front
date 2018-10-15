cf6734ce7c8b7b758bee6c7a5a1b7bdb9b520e7e
FabienSchlegel <fabien.schlegel@omnilog.fr>
2018-10-15 13:58:39 +0200

    update places routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,47 +1,15 @@
 import express from 'express';
 
-import Sequelize from 'sequelize';
-
-import { Place, Voice } from '../sequelize';
-
 import voteRouter from './votes'
 import voiceRouter from './voices'
+import placeRouter from './places'
 
 const router = express.Router();
-
-const Op = Sequelize.Op;
 
 router.use('/vote', voteRouter);
 
 router.use('/voice', voiceRouter);
 
-// Return a list of places. Need 4 coordinates
-router.get('/places/list', (req, res) => {
-
-  if (req.query.ne_lat && req.query.ne_lng && req.query.sw_lat && req.query.sw_lng) {
-
-    Place.findAll({
-      where: {
-        lat: {
-          [Op.between]: [req.query.ne_lat, req.query.sw_lat]
-        },
-        lng: {
-          [Op.between]: [req.query.ne_lng, req.query.sw_lng]
-        },
-      }
-    }).then(places => {
-
-      if (places.length === 0) {
-
-        res.json([])
-      } else {
-        res.json(places)
-      }
-    })
-
-  } else {
-    res.sendStatus(400);
-  }
-});
+router.use('/place', placeRouter)
 
 export default router;
---
+++ api/routes/places/index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import Sequelize from 'sequelize';
+
+import { Place } from '../../sequelize';
+
+const Op = Sequelize.Op;
+
+const router = express.Router();
+
+// Return a list of places. Need 4 coordinates
+router.get('/list', (req, res) => {
+
+    if (req.query.ne_lat && req.query.ne_lng && req.query.sw_lat && req.query.sw_lng) {
+  
+      Place.findAll({
+        where: {
+          lat: {
+            [Op.between]: [req.query.ne_lat, req.query.sw_lat]
+          },
+          lng: {
+            [Op.between]: [req.query.ne_lng, req.query.sw_lng]
+          },
+        }
+      }).then(places => {
+  
+        if (places.length === 0) {
+  
+          res.json([])
+        } else {
+          res.json(places)
+        }
+      })
+  
+    } else {
+      res.sendStatus(400);
+    }
+  });
+
+export default router;
