d522cd2c6948a00716fb613180b53ff2ea700b76
FabienSchlegel <fabien.schlegel@omnilog.fr>
2018-10-16 17:28:39 +0200

    add About component

---
+++ front/src/components/About.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { Container, Row, Col } from 'reactstrap'
+
+const APropos = () => {
+    return (
+        <Container className="APropos" fluid>
+            <Row noGutters className="justify-content-center align-items-center w-100 pt-5">
+                <Col xs="12" lg="6" className="bg-lighter round-corners px-3">
+
+                    <Row noGutters className="justify-content-center align-items-center h-100 pt-5">
+                        <Col>
+                            <h1 className="text-center">A propos...</h1>
+                        </Col>
+                    </Row>
+                    <Row noGutters className="justify-content-center align-items-center h-100 py-5">
+                        <Col>
+                            <p className="text-center">
+                                Ce site est réalisé avec React Redux, servi par NodeJS avec une base de données Postgresql.<br></br>
+                                Il est hébergé grâce à Heroku.
+                            </p>
+                            <p className="text-center">
+                                Le code est hébergé sur Github, il est visible <a href="https://github.com/Humch/vote4lunch" className="text-white">ici</a>
+                            </p>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default APropos;
