db89e038933e8d4e2a5fa448334ea7662f1bc636
FabienSchlegel <fabien.schlegel@omnilog.fr>
2018-10-16 13:32:41 +0200

    reorganize files for heroku deployment

--- app.js
+++ app.js
@@ -27,33 +27,15 @@ app.use(bodyParser.urlencoded({
 
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '/../front/build')));
+app.use(express.static(path.join(__dirname, '/front/build')));
 
 app.use('/api', index);
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/../front/build/index.html'));
+  res.sendFile(path.join(__dirname+'/front/build/index.html'));
 });
-
-// // catch 404 and forward to error handler
-// app.use((req, res, next) => {
-//   const err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// // error handler
-// /* eslint no-unused-vars: 0 */
-// app.use((err, req, res, next) => {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 // Handle uncaughtException
 process.on('uncaughtException', (err) => {
