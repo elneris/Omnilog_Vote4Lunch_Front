8109c90a7801358bc6e688f97fcfb6b548be0d14
FabienSchlegel <fabien.schlegel@omnilog.fr>
2018-10-16 13:34:06 +0200

    update app.js to serve React files

--- app.js
+++ app.js
@@ -27,14 +27,14 @@ app.use(bodyParser.urlencoded({
 
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '/front/build')));
+app.use(express.static(path.join(__dirname, 'front/build')));
 
 app.use('/api', index);
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/front/build/index.html'));
+  res.sendFile(path.join(__dirname + 'front/build/index.html'));
 });
 
 // Handle uncaughtException
