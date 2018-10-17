c9255f1fbe221ea1c23366c0d6343bd0003f5968
Humch <fabienschlegel@yahoo.fr>
2018-10-17 09:40:56 +0200

    update PlaceCard

--- front/src/containers/AddAVoice.jsx
+++ front/src/containers/AddAVoice.jsx
@@ -10,77 +10,76 @@ import VoteMap from './VoteMap';
 import LoginModal from './LoginModal';
 
 class AddAVoice extends Component {
+  constructor() {
+    super();
 
-    constructor() {
-        super()
+    this.state = {
+      openLoginModal: false
+    };
+  }
 
-        this.state = {
-            openLoginModal: false
-        }
-    } 
+  componentDidMount() {
+    this.props.dispatch(getPlacesList(this.props.match.params.url));
+    const pseudo = localStorage.getItem('pseudo');
+    const email = localStorage.getItem('email');
 
-    componentDidMount() {
-        this.props.dispatch(getPlacesList(this.props.match.params.url));
-        const pseudo = localStorage.getItem('pseudo');
-        const email = localStorage.getItem('email');
+    if (!pseudo || !email) {
+      this.setState({
+        openLoginModal: true
+      })
+    } else {
+      this.setState({
+        openLoginModal: false
+      })
+    }
+  }
 
-        if(!pseudo || !email) {
-            this.setState({
-                openLoginModal: true
-            }) 
-        } else {
-            this.setState({
-                openLoginModal: false
-            })
-        }
+  render() {
+    const { restaurants } = this.props;
+
+    let listOfRestaurants;
+
+    if (restaurants[this.props.match.params.url]) {
+      listOfRestaurants = restaurants[this.props.match.params.url]
+    } else {
+      listOfRestaurants = [];
     }
 
-    render() {
-        const { restaurants } = this.props
+    return (
 
-        let listOfRestaurants
-        
-        if(restaurants[this.props.match.params.url]) {
-            listOfRestaurants = restaurants[this.props.match.params.url]
-        } else {
-            listOfRestaurants = []
-        }
+      <Container fluid className="AddVoice">
 
-        return (
+        <Row className="justify-content-center pt-3">
 
-            <Container fluid className="AddVoice">
+          {listOfRestaurants.map(restaurant => (
+            <PlaceCard
+              key={restaurant.id}
+              restaurant={restaurant}
+              vote_url={this.props.match.params.url}
+            />
+          ))}
+        </Row>
+        <Row
+          className="justify-content-center align-items-center"
+          noGutters
+        >
+          <Col
+            xs="8"
 
-                <Row className="justify-content-center align-items-center pt-3">
-
-                    {listOfRestaurants.map(restaurant => (
-                        <PlaceCard
-                            key={restaurant.id}
-                            restaurant={restaurant}
-                            vote_url={this.props.match.params.url}
-                        />
-                    ))}
-                </Row>
-                <Row
-                    className="justify-content-center align-items-center"
-                    noGutters
-                >
-                    <Col
-                        xs="8"
-
-                    >
-                        <VoteMap restaurants={listOfRestaurants} />
-                    </Col>
-                </Row>
-                {this.state.openLoginModal ? <LoginModal open /> : ''}
-            </Container>
+          >
+            <VoteMap restaurants={listOfRestaurants} />
+          </Col>
+        </Row>
+        {this.state.openLoginModal ? <LoginModal open /> : ''}
+      </Container>
 
 
-        );
-    }
+    );
+  }
 }
 
 const mstp = ({ getPlacesList }) => ({
-    restaurants: getPlacesList.result,
+  restaurants: getPlacesList.result,
 });
 
 export default connect(mstp)(AddAVoice);
