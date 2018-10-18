e830404198543f621fde64521cd47321c4783f7e
FabienSchlegel <fabien.schlegel@omnilog.fr>
2018-10-18 11:05:00 +0200

    update for eslint

--- front/src/actions/getUsersVotes.js
+++ front/src/actions/getUsersVotes.js
@@ -1,29 +1,29 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export const getUsersVotesBegin = () => ({
-    type: 'GET_USERS_VOTES_BEGIN',
+  type: 'GET_USERS_VOTES_BEGIN',
 });
 
-export const getUsersVotesSuccess = (votes) => ({
-    type: 'GET_USERS_VOTES_SUCCESS',
-    votes,
+export const getUsersVotesSuccess = votes => ({
+  type: 'GET_USERS_VOTES_SUCCESS',
+  votes,
 });
 
 export const getUsersVotesFailure = error => ({
-    type: 'GET_USERS_VOTES_FAILURE',
-    error,
+  type: 'GET_USERS_VOTES_FAILURE',
+  error,
 });
 
 export function getUsersVotes(pseudo) {
-    return (dispatch) => {
-        dispatch(getUsersVotesBegin());
-        return (
-            axios
-                .get(`/api/vote/get/mine?pseudo=${pseudo}`)
-                .then(votes => {
-                    dispatch(getUsersVotesSuccess(votes.data))
-                } )
-                .catch(error => dispatch(getUsersVotesSuccess(error)))
-        );
-    };
+  return (dispatch) => {
+    dispatch(getUsersVotesBegin());
+    return (
+      axios
+        .get(`/api/vote/get/mine?pseudo=${pseudo}`)
+        .then((votes) => {
+          dispatch(getUsersVotesSuccess(votes.data));
+        })
+        .catch(error => dispatch(getUsersVotesSuccess(error)))
+    );
+  };
 }
