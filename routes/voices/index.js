5e5aa0522e4e81ced11f2025e94f42f0e6b2fa8f
FabienSchlegel <fabien.schlegel@omnilog.fr>
2018-10-18 11:18:40 +0200

    update for eslint

--- routes/voices/index.js
+++ routes/voices/index.js
@@ -13,50 +13,50 @@ router.post('/add', (req, res) => {
       pseudo: req.body.pseudo,
       email: req.body.email,
     }).then(() => {
-      res.json({ vote: true })
-    })
+      res.json({ vote: true });
+    });
   } else {
-    res.sendStatus(400)
-  }    
+    res.sendStatus(400);
+  }
 });
-  
-  // Return the sum of voices for a vote and a place
-  router.get('/count/all', (req, res) => {
-    Voice.findAndCountAll({
-      where: {
-        voteId: req.query.vote_id,
-        placeId: req.query.place_id
-      }
-    }).then(voices=>{
-      res.json(voices)
-    })
+
+// Return the sum of voices for a vote and a place
+router.get('/count/all', (req, res) => {
+  Voice.findAndCountAll({
+    where: {
+      voteId: req.query.vote_id,
+      placeId: req.query.place_id
+    }
+  }).then((voices) => {
+    res.json(voices);
   });
+});
 
 // Verify if a user have voted for a vote
-router.get('/count/foruser', (req,res) => {
+router.get('/count/foruser', (req, res) => {
   Voice.findAndCountAll({
     where: {
       voteId: req.query.vote_id,
       pseudo: req.query.pseudo,
       email: req.query.email
     }
-  }).then(result=>{
+  }).then((result) => {
     if (result.count === 0) {
       res.json({
-        'vote_id':req.query.vote_id,
-        'pseudo': req.query.pseudo,
-        'email': req.query.email,
-        'vote': false,
-      })
+        vote_id: req.query.vote_id,
+        pseudo: req.query.pseudo,
+        email: req.query.email,
+        vote: false,
+      });
     } else {
       res.json({
-        'vote_id':req.query.vote_id,
-        'pseudo': req.query.pseudo,
-        'email': req.query.email,
-        'vote': true,
-      })
+        vote_id: req.query.vote_id,
+        pseudo: req.query.pseudo,
+        email: req.query.email,
+        vote: true,
+      });
     }
-  })
+  });
 });
 
 export default router;
