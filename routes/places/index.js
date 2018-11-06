9525e4f8bc0212bfd5bda918b501fde4bece65e0
Fabien Schlegel <fabien.schlegel@omnilog.fr>
2018-11-06 13:54:51 +0100

    update tests for routes update routes for eslint

--- routes/places/index.js
+++ routes/places/index.js
@@ -3,37 +3,32 @@ import Sequelize from 'sequelize';
 
 import { Place } from '../../sequelize';
 
-const Op = Sequelize.Op;
+const { Op } = Sequelize;
 
 const router = express.Router();
 
 // Return a list of places. Need 4 coordinates
 router.get('/list', (req, res) => {
-
-    if (req.query.ne_lat && req.query.ne_lng && req.query.sw_lat && req.query.sw_lng) {
-  
-      Place.findAll({
-        where: {
-          lat: {
-            [Op.between]: [req.query.ne_lat, req.query.sw_lat]
-          },
-          lng: {
-            [Op.between]: [req.query.ne_lng, req.query.sw_lng]
-          },
-        }
-      }).then(places => {
-  
-        if (places.length === 0) {
-  
-          res.json([])
-        } else {
-          res.json(places)
-        }
-      })
-  
-    } else {
-      res.sendStatus(400);
-    }
-  });
+  if (req.query.ne_lat && req.query.ne_lng && req.query.sw_lat && req.query.sw_lng) {
+    Place.findAll({
+      where: {
+        lat: {
+          [Op.between]: [req.query.ne_lat, req.query.sw_lat]
+        },
+        lng: {
+          [Op.between]: [req.query.ne_lng, req.query.sw_lng]
+        },
+      }
+    }).then((places) => {
+      if (places.length === 0) {
+        res.json([]);
+      } else {
+        res.json(places);
+      }
+    });
+  } else {
+    res.sendStatus(400);
+  }
+});
 
 export default router;
