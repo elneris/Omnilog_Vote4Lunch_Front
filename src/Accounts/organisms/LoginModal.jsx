6e412936bd72baecb5fa345415c467339c0f8d76
Fabien Schlegel <fabien.schlegel@omnilog.fr>
2018-11-07 16:15:16 +0100

    update LoginModal container

--- src/Accounts/organisms/LoginModal.jsx
+++ src/Accounts/organisms/LoginModal.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input } from 'reactstrap';
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+} from 'reactstrap';
+
+import { FormInputPseudo, FormInputEmail } from '..';
 
 import { updateUserData } from '../actions';
 import { getUserVoices } from '../../Votes/actions';
@@ -13,83 +22,63 @@ class LoginModal extends Component {
 
     this.state = {
       modal: false,
-      pseudo: '',
-      email: '',
     };
 
     this.toggle = this.toggle.bind(this);
   }
 
   componentDidMount() {
-    if (this.props.open) {
+    const { open } = this.props;
+    if (open) {
       this.toggle();
     }
   }
 
   componentDidUpdate() {
-    if (!this.state.modal) {
-      this.props.dispatch(getUserVoices(
-        this.state.pseudo,
-        this.state.email,
-        [this.props.voteUrl]
-      ));
+    const {
+      pseudo,
+      email,
+      voteUrl,
+      getUserVoices: getUV,
+    } = this.props;
+    const { modal } = this.state;
+    if (!modal) {
+      getUV(
+        pseudo,
+        email,
+        [voteUrl]
+      );
     }
   }
 
   toggle() {
+    const { modal } = this.state;
     this.setState({
-      modal: !this.state.modal
-    });
-  }
-
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  handleDateChange(date) {
-    this.setState({
-      date,
+      modal: !modal,
     });
   }
 
   submitForm(e) {
+    const { pseudo, email, updateUserData: updateUD } = this.props;
     e.preventDefault();
-    localStorage.setItem('pseudo', this.state.pseudo);
-    localStorage.setItem('email', this.state.email);
+    localStorage.setItem('pseudo', pseudo);
+    localStorage.setItem('email', email);
     this.setState({
       modal: false,
     });
-    this.props.dispatch(updateUserData(this.state.pseudo, this.state.email));
+    updateUD(pseudo, email);
   }
 
   render() {
+    const { modal } = this.state;
+    const { className } = this.props;
     return (
-      <Modal isOpen={this.state.modal} className={this.props.className}>
+      <Modal isOpen={modal} className={className}>
         <ModalHeader>Présente toi avant de voter</ModalHeader>
         <ModalBody>
           <Form onSubmit={e => this.submitForm(e)}>
-            <FormGroup>
-              <Label for="pseudo">Indique ton pseudo</Label>
-              <Input
-                type="text"
-                name="pseudo"
-                id="pseudo"
-                value={this.state.pseudo}
-                onChange={e => this.handleChange(e)}
-                required
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label for="email">Indique ton email</Label>
-              <Input
-                type="email"
-                name="email"
-                id="email"
-                value={this.state.email}
-                onChange={e => this.handleChange(e)}
-                required
-              />
-            </FormGroup>
+            <FormInputPseudo />
+            <FormInputEmail />
             <div className="text-center mt-5">
               <Button color="success">Bonjour</Button>
             </div>
@@ -101,10 +90,20 @@ class LoginModal extends Component {
 }
 
 LoginModal.propTypes = {
-  dispatch: PropTypes.func,
+  updateUserData: PropTypes.func,
+  getUserVoices: PropTypes.func,
   open: PropTypes.bool,
   voteUrl: PropTypes.string,
   className: PropTypes.string,
+  pseudo: PropTypes.string,
+  email: PropTypes.string,
 };
 
-export default connect()(LoginModal);
+const mstp = ({ userData }) => ({
+  pseudo: userData.pseudo,
+  email: userData.email,
+});
+
+const mdtp = dispatch => bindActionCreators({ getUserVoices, updateUserData }, dispatch);
+
+export default connect(mstp, mdtp)(LoginModal);
