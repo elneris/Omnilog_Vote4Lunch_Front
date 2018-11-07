ebb36e998b43938671d64dd4f24ae6c4bc51956c
Fabien Schlegel <fabien.schlegel@omnilog.fr>
2018-11-07 16:42:39 +0100

    update userData reducer and tests

--- src/Accounts/__tests__/AccountsReducers.spec.js
+++ src/Accounts/__tests__/AccountsReducers.spec.js
@@ -2,14 +2,33 @@
 import userDataReducer from '../reducers';
 
 describe('Test Accounts Reducers', () => {
+  let state;
+
+  beforeEach(() => {
+    state = { pseudo: 'bob', email: 'bob@bob.com' };
+  });
+
   it('UPDATE_USER_DATA case reducer for userData', () => {
-    let state = { pseudo: 'bob', email: 'bob@bob.com' };
-    state = userDataReducer(state, { type: 'UPDATE_USER_DATA', pseudo: 'bob', email: 'bob@bob.com' });
-    expect(state).toEqual({ pseudo: 'bob', email: 'bob@bob.com' });
+    state = userDataReducer(state, { type: 'UPDATE_USER_DATA', pseudo: 'roger', email: 'roger@roger.com' });
+    expect(state).toEqual({ pseudo: 'roger', email: 'roger@roger.com' });
+  });
+
+  it('FORM_INPUT_PSEUDO case reducer for userData', () => {
+    state = userDataReducer(state, { type: 'FORM_INPUT_PSEUDO', pseudo: 'roger' });
+    expect(state).toEqual({ pseudo: 'roger', email: 'bob@bob.com' });
+  });
+
+  it('UPDATE_USER_DATA case reducer for userData', () => {
+    state = userDataReducer(state, { type: 'FORM_INPUT_EMAIL', email: 'roger@roger.com' });
+    expect(state).toEqual({ pseudo: 'bob', email: 'roger@roger.com' });
+  });
+
+  it('RESET_VOTE_DATA case reducer for userData', () => {
+    state = userDataReducer(state, { type: 'RESET_VOTE_DATA' });
+    expect(state).toEqual({ pseudo: '', email: '' });
   });
 
   it('default case reducer for userData', () => {
-    let state = { pseudo: 'bob', email: 'bob@bob.com' };
     state = userDataReducer(state, { type: '' });
     expect(state).toEqual({ pseudo: 'bob', email: 'bob@bob.com' });
   });
