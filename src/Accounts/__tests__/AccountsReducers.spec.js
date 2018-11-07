6ae88fdb62be504bb289e9034c5722e7db81af68
Fabien Schlegel <fabien.schlegel@omnilog.fr>
2018-11-07 14:59:37 +0100

    update Accounts reducer tests

--- src/Accounts/__tests__/AccountsReducers.spec.js
+++ src/Accounts/__tests__/AccountsReducers.spec.js
@@ -2,9 +2,15 @@
 import userDataReducer from '../reducers';
 
 describe('Test Accounts Reducers', () => {
-  it('reducer for userData', () => {
+  it('UPDATE_USER_DATA case reducer for userData', () => {
     let state = { pseudo: 'bob', email: 'bob@bob.com' };
     state = userDataReducer(state, { type: 'UPDATE_USER_DATA', pseudo: 'bob', email: 'bob@bob.com' });
+    expect(state).toEqual({ pseudo: 'bob', email: 'bob@bob.com' });
+  });
+
+  it('default case reducer for userData', () => {
+    let state = { pseudo: 'bob', email: 'bob@bob.com' };
+    state = userDataReducer(state, { type: '' });
     expect(state).toEqual({ pseudo: 'bob', email: 'bob@bob.com' });
   });
 });
